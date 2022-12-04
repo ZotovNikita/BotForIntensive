d063ee8209ba3339a659e02ed4bba3af8caa8181
Nikita Zotov <usernamex821@gmail.com>
2022-12-04 21:21:08 +0600

    backend

--- backend/back.py
+++ backend/back.py
@@ -1,8 +1,9 @@
 from http.client import HTTPException
-from typing import Union
+from typing import List, Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 import yaml
 import json
@@ -21,6 +22,9 @@ class QuestionModel(BaseModel):
     name: str
     examples: list
     answer: str
+
+class AllQuestionModel(BaseModel):
+    intents: List[QuestionModel]
 
 with open("data.json", encoding='utf-8') as f:
     data = json.load(f)
@@ -60,7 +64,6 @@ def add_intent(intent: QuestionModel):
 
 @app.delete("/delite_intent")
 def delite_intent(name):
- 
     with open("../domain.yml", encoding='utf-8') as dom:
         d = yaml.load(dom, Loader=yaml.Loader)
         if name not in d['intents']:
@@ -91,15 +94,34 @@ def delite_intent(name):
     with open('../data/rules.yml', 'w', encoding='utf-8') as f:
         f.write(yaml.dump(r, sort_keys=False, allow_unicode=True))
 
-@app.get("/get_intent/{intent.name}", response_model=QuestionModel)
-async def get_intent(intent: QuestionModel):
-    return intent[intent.name].json()
+@app.get("/get_intent/")  
+async def get_intent():
+    with open("../domain.yml", encoding='utf-8') as dom:
+        d = yaml.load(dom, Loader=yaml.Loader)
+        dom.close()
+    with open("../data/nlu.yml", encoding='utf-8') as nlu:
+        n = yaml.load(nlu, Loader=yaml.Loader)
+        nlu.close()
+    js = {'intent_objects':[]}
+    for i in range(len(d['intents'])):
+        js_part = {'name': n['nlu'][i]['intent'], 'examples': n['nlu'][i]['examples'].split('\n'), 'answer': d['responses']['utter_' + n['nlu'][i]['intent']][0]['text']}
+        js['intent_objects'].append(js_part)
+    return JSONResponse(content=jsonable_encoder(js))
     
-@app.put("/update_intent/{intent.name}", response_model=QuestionModel)
-async def update_intent(intent: QuestionModel):
-    update_name_encoded = jsonable_encoder(intent.name)
-    intent[intent.name] = update_name_encoded
-    return update_name_encoded
+@app.put("/update_intent/")
+def update_intent(intent: QuestionModel):
+    with open("../domain.yml", encoding='utf-8') as dom:
+        d = yaml.load(dom, Loader=yaml.Loader)
+        dom.close()
+    if intent.name not in d['intents']:
+        raise HTTPException(status_code=404, detail="Среди имеющихся блоков вопросов нет блока с таким названием. Возможно, вы хотите создать новый блок?")
+    if len(intent.examples) == 0:
+        raise HTTPException(status_code=404, detail="Поле возможных вопросов от пользователя не может быть пустым")
+    if len(intent.answer) == 0:
+        raise HTTPException(status_code=404, detail="Поле ответа на вопросы не может быть пустым")
+    delite_intent(intent.name)
+    add_intent(intent)
+    
 
 # @app.get("/train")
 # async def get_intent():
